0cd37ee411624cff1bacc4bd5c5865e1e1485db0
Ashish <ashishkumarmaurya794@gmail.com>
2026-07-22 21:03:37 +0530

    nav bar desging

--- tripmitra/src/Components/Navbar/Navbar.jsx
+++ tripmitra/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from "react";
+import logo from "../../Assets/logo.png";
+import {CircleUserRound} from "lucide-react";
+import styles from "./Navbar.module.css";
+
 
 function Navbar() {
-    const Navitem=["Home",""]
+  const Navitem = ["Home", "Features", "Destination", "About"];
   return (
     <div>
-      
+      <div className={styles.navbar}>
+        <div className={styles.logo}>
+          <img src={logo} alt="logo" />
+          <div className={styles.name}  >TripMitra</div>
+        </div>
+
+        <div className={styles.middle}>{Navitem.map((content) => content)}</div>
+
+        <div className={styles.topRight}>
+          <button className={styles.trip} >Plan a Trip</button>
+          <CircleUserRound />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
