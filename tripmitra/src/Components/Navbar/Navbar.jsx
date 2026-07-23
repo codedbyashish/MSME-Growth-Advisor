6552c0d5d05828b826a4a84e92a2500ff6671e21
Ashish <ashishkumarmaurya794@gmail.com>
2026-07-23 23:01:42 +0530

    navbar is working

--- tripmitra/src/Components/Navbar/Navbar.jsx
+++ tripmitra/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import logo from "../../Assets/logo.png";
-import {CircleUserRound} from "lucide-react";
+import { CircleUserRound } from "lucide-react";
 import styles from "./Navbar.module.css";
-
+import {Link,NavLink} from 'react-router-dom'
 
 function Navbar() {
   const Navitem = ["Home", "Features", "Destination", "About"];
@@ -11,13 +11,24 @@ function Navbar() {
       <div className={styles.navbar}>
         <div className={styles.logo}>
           <img src={logo} alt="logo" />
-          <div className={styles.name}  >TripMitra</div>
+          <div className={styles.name}>TripMitra</div>
         </div>
 
-        <div className={styles.middle}>{Navitem.map((content) => content)}</div>
+        <div className={styles.middle}>
+          <Navlink className={
+            ({isActive})=>{
+              {isActive ?"text-blue-500":"text-black"}
+
+            }
+          }>
+            {Navitem.map((content, index) => (
+              <p key={index}>{content}</p>
+            ))}
+          </Navlink>
+        </div>
 
         <div className={styles.topRight}>
-          <button className={styles.trip} >Plan a Trip</button>
+          <button className={styles.trip}>Plan a Trip</button>
           <CircleUserRound />
         </div>
       </div>
